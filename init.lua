fd09633a360998f5afbcc1fe0e784eadea4904f9
wduf <wduf02@gmail.com>
2026-02-07 01:34:11 -0500

    add wq

--- init.lua
+++ init.lua
@@ -50,6 +50,7 @@ end
 
 map("n", "<leader>w", ":w<CR>")
 map("n", "<leader>q", ":q<CR>")
+map("n", "<leader>wq", ":wq<CR>")
 map("n", "<leader>n", ":enew<CR>")
 map("n", "<leader>e", ":Ex<CR>")
 map("n", "<leader>f", "mmggVG=`mzz") -- TODO: replace with real formatter
@@ -57,7 +58,7 @@ map("n", "<leader>f", "mmggVG=`mzz") -- TODO: replace with real formatter
 map("n", "<leader>bn", ":bn<CR>")
 map("n", "<leader>bp", ":bp<CR>")
 map("n", "<leader>bd", ":bd<CR>")
-map("n", "<leader>bl", ":buffers<CR>") -- TODO: tbd if needed
+map("n", "<leader>bl", ":buffers<CR>")
 
 map("n", "<leader>s", ":vsplit<CR>")
 map("n", "<leader>c", ":close<CR>")
