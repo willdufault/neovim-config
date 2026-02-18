fcc5bd36f5ccf9006d1034865ce4f7a9d73be502
Will Dufault <wduf02@gmail.com>
2026-02-18 09:03:39 -0500

    Configure lualine to show full file path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,12 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {}
+		opts = {
+			sections = {
+				-- Show full path
+				lualine_c = { { "filename", path = 2 } },
+			},
+		},
 	},
 }
+j
