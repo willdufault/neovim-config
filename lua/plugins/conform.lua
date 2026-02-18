e07c04c4a4b1884f0fa64203fed347c34bb2f5e5
Will Dufault <wduf02@gmail.com>
2026-02-18 10:43:27 -0500

    update prettier & stylua options

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,33 +1,41 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = {},
-		config = function()
-			require("conform").setup({
-				formatters = {
-					prettier = {
-						append_args = {
-							"--no-semi",
-						},
-					},
-				},
-				formatters_by_ft = {
-					c = { "clang-format" },
-					css = { "prettier" },
-					html = { "prettier" },
-					javascript = { "prettier" },
-					javascriptreact = { "prettier" },
-					json = { "prettier" },
-					lua = { "stylua" },
-					python = { "isort", "black" },
-					typescript = { "prettier" },
-					typescriptreact = { "prettier" },
-					yaml = { "prettier" },
-				},
-			})
-			vim.keymap.set("n", "<leader>f", function()
-				require("conform").format({ async = true, lsp_format = "fallback" })
-			end)
-		end,
-	},
+  {
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+      require("conform").setup({
+        formatters = {
+          prettier = {
+            append_args = {
+              "--no-semi",
+              "--trailing-comma=none",
+            },
+          },
+          stylua = {
+            append_args = {
+              "--indent-type=Spaces",
+              "--indent-width=2",
+              "--column-width=80",
+            },
+          },
+        },
+        formatters_by_ft = {
+          c = { "clang-format" },
+          css = { "prettier" },
+          html = { "prettier" },
+          javascript = { "prettier" },
+          javascriptreact = { "prettier" },
+          json = { "prettier" },
+          lua = { "stylua" },
+          python = { "isort", "black" },
+          typescript = { "prettier" },
+          typescriptreact = { "prettier" },
+          yaml = { "prettier" },
+        },
+      })
+      vim.keymap.set("n", "<leader>f", function()
+        require("conform").format({ async = true, lsp_format = "fallback" })
+      end)
+    end,
+  },
 }
