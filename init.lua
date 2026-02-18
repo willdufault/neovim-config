1b3d9e5846629366b2b015c6cf59f2dd82d32a55
Will Dufault <wduf02@gmail.com>
2026-02-18 10:44:46 -0500

    Reorder Lazy setup and keymap configuration
    Set leader keys before Lazy setup for correct keymaps.

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- Set before Lazy setup to ensure correct keymaps
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+require("config.lazy")
 require("config.preferences")
 require("config.keymaps")
-require("config.lazy")
