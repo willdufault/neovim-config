16e5b1d750448e2a53e57bd5d5eed50f3ce3acbc
wduf <wduf02@gmail.com>
2026-02-23 15:24:51 -0500

    add treesitter context; change telescope + split binds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,10 +13,10 @@ return {
     end
 
     local builtin = require("telescope.builtin")
-    map("<leader>ff", builtin.find_files)
-    map("<leader>fg", builtin.live_grep)
-    map("<leader>fb", builtin.buffers)
-    map("<leader>fh", builtin.help_tags)
-    map("<leader>fs", builtin.lsp_document_symbols, { buffer = args.buf })
+    map("<leader>sf", builtin.find_files)
+    map("<leader>sg", builtin.live_grep)
+    map("<leader>sb", builtin.buffers)
+    map("<leader>sh", builtin.help_tags)
+    map("<leader>ss", builtin.lsp_document_symbols, { buffer = args.buf })
   end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/nvim-treesitter-context" },
     lazy = false,
     build = ":TSUpdate",
     config = function()
