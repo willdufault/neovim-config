6293e507813f5f8dcebb95d32f6ca0b33d74901a
wduf <wduf02@gmail.com>
2026-03-11 22:16:23 -0400

    add back treesitter + autotag

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/nvim-treesitter-context" },
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      local filetypes = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "javascript",
+        "json",
+        "jsx",
+        "lua",
+        "luadoc",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      }
+      require("nvim-treesitter").install(filetypes)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = filetypes,
+        callback = function()
+          -- Enable syntax highlighting
+          vim.treesitter.start()
+          -- Enable smart folding
+          vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.wo.foldmethod = "expr"
+          vim.wo.foldlevel = 99
+          -- Enable smart indentation
+          -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+  },
+}
