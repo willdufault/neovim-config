912a337d13c5efca6a11156a7cb6d58d6db9cc8e
wduf <wduf02@gmail.com>
2026-03-20 19:20:40 -0400

    remove diff from lualine B

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,7 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     sections = {
+      lualine_b = { "branch", "diagnostics" },
       lualine_c = { { "filename", path = 2 } },
       lualine_x = { "filetype" },
     },
